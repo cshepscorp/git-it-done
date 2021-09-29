618bf3cb14bb3b221d4cd54879703ca5dc34c1b2
cshepscorp <sheppard.christy@gmail.com>
2021-09-28 21:25:51 -0400

    list issues initial commit

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -6,6 +6,7 @@ var repoSearchTerm = document.querySelector("#repo-search-term");
 var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
+  
 
   // make a request to the url
   fetch(apiUrl)
