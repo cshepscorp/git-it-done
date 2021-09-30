c129439abe922daf1d30acea397bc49e9659c772
cshepscorp <sheppard.christy@gmail.com>
2021-09-30 12:25:05 -0400

    can search by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,22 @@ var userFormEl = document.querySelector("#user-form"); // search area's form id
 var nameInputEl = document.querySelector("#username"); // input area's id
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data.items, language);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
+};
+
 
 var getUserRepos = function(user) {
   // format the github api url
@@ -89,5 +105,19 @@ var displayRepos = function(repos, searchTerm) {
   }
 
 }
+
+var buttonClickHandler = function(event){
+  var language = event.target.getAttribute("data-language");
+  console.log(language);
+
+  if (language) {
+    getFeaturedRepos(language);
   
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+}
+
+getFeaturedRepos();
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
